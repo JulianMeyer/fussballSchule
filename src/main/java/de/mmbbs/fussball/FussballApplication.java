c7d847d821ce04733500e44fbea6943c2db3761c
Julian.Meyer <julian.meyer@rossmann.de>
2022-11-07 15:02:32 +0100

    First commit

---
+++ src/main/java/de/mmbbs/fussball/FussballApplication.java
@@ -0,0 +1,13 @@
+package de.mmbbs.fussball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FussballApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FussballApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/mmbbs/fussball/FussballApplicationTests.java
@@ -0,0 +1,13 @@
+package de.mmbbs.fussball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FussballApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
